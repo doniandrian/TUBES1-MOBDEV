84bc96a46ef84beae6e776da2c16473f796f51cc
Kevin Jo <kev27jo@gmail.com>
2023-11-12 23:01:07 +0700

    update layout fragment left, setting, font size popup

--- TUBES/app/build.gradle.kts
+++ TUBES/app/build.gradle.kts
@@ -14,7 +14,6 @@ android {
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -60,7 +59,9 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference:1.2.1")
     implementation ("com.google.code.gson:gson:2.8.8")
+    implementation ("com.airbnb.android:lottie:3.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- TUBES/app/src/main/java/com/example/tubes/SettingFragment.kt
+++ TUBES/app/src/main/java/com/example/tubes/SettingFragment.kt
@@ -1,18 +1,25 @@
 package com.example.tubes
 
-import android.annotation.SuppressLint
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Switch
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
+import com.example.tubes.databinding.ListItemFontSizeBinding
 import com.example.tubes.databinding.SettingsBinding
+
 
 class SettingFragment : Fragment() {
     private lateinit var binding: SettingsBinding
+    private lateinit var binding2: ListItemFontSizeBinding
     private lateinit var btn_font_size : ImageView
     private lateinit var btn_about : ImageView
     private lateinit var btn_close : ImageView
@@ -20,17 +27,18 @@ class SettingFragment : Fragment() {
 
     private lateinit var switch_dark_mode : Switch
     private lateinit var switch_display_time : Switch
-
+    private lateinit var btnPopUp : Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-
     ): View? {
         binding = SettingsBinding.inflate(inflater, container, false)
+        binding2 = ListItemFontSizeBinding.inflate(inflater, container, false)
         btn_font_size = binding.fontSize
         btn_about = binding.About
         switch_dark_mode = binding.switchDarkMode
         switch_display_time = binding.switchDisplayTime
+        btn_close = binding2.close
 
 
 
@@ -42,7 +50,7 @@ class SettingFragment : Fragment() {
             activity.supportFragmentManager.beginTransaction().add(R.id.fontpopup,ListFontSizeFragment()
             )
                 .show(ListFontSizeFragment()).commit()
-
+                showDialog()
 
         }
 
@@ -64,4 +72,33 @@ class SettingFragment : Fragment() {
         }
         return binding.root
     }
+    private fun showDialog() {
+        val overlay = View(requireContext())
+        overlay.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        overlay.setBackgroundColor(Color.parseColor("#80000000"))
+
+        val dialog = Dialog(requireContext())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.setContentView(R.layout.list_item_font_size)
+
+        val width = ViewGroup.LayoutParams.MATCH_PARENT
+        val height = ViewGroup.LayoutParams.WRAP_CONTENT
+        dialog.window?.setLayout(width, height)
+
+        val closeButton = dialog.findViewById<ImageView>(R.id.close)
+        closeButton.setOnClickListener {
+            dialog.dismiss()
+            (requireActivity().findViewById<ViewGroup>(android.R.id.content)).removeView(overlay)
+        }
+        (requireActivity().findViewById<ViewGroup>(android.R.id.content)).addView(overlay)
+
+        dialog.show()
+    }
+
+
 }
+
